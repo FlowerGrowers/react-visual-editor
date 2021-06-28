9f97336313bfb1906d30e21e31eb130306183ede
jiajunda <1007274033@qq.com>
2021-06-28 22:23:30 +0800

    clean project

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,12 @@
-import JsonData from './edit-data.json'
-import {ReactVisualEditor} from "./packages/ReactVisualEditor";
-import './app.scss'
-import {useState} from "react";
-import {visualEditorBaseOption} from "./visual.config";
-import {Input, notification} from 'antd'
+import {useState} from 'react';
+import "./app.scss";
 
-function App() {
-
-    const [formData, setFormData] = useState({
-        username: 'hello world',
-        maxLevel: 200,
-        minLevel: 100,
-    } as any)
-    const [customProps] = useState({
-        submitButton: {
-            onClick: () => {
-                notification.info({message: '执行提交表单逻辑'})
-            }
-        },
-        changeField: {
-            onChange: (e: any) => {
-                notification.info({message: e.target.value})
-            }
-        },
-    })
-
+const APP = () => {
     return (
-        <div className="app">
-            <ReactVisualEditor formData={formData}
-                               option={visualEditorBaseOption}
-                               value={JsonData as any}
-                               customBlockProps={customProps}
-                               onUpdateFormData={setFormData}>
-                {{
-                    editUsername: ({model}) => {
-                        return (
-                            formData.food !== 'dangao' ?
-                                <input type="text" {...model.modelValue}/> :
-                                <Input {...model.modelValue}/>
-                        )
-                    }
-                }}
-            </ReactVisualEditor>
-            <div style={{textAlign: 'center'}}>
-                {JSON.stringify(formData)}
-            </div>
+        <div className={'app-home'}>
+            hello world!
         </div>
     )
 }
 
-export default App
+export default APP;
